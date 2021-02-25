df5fa6c79d54b586c8b28abfa0ad333b3ed0ed88
EnkhAmar <enkhamar1220@gmail.com>
2021-02-26 06:14:10 +0800

    final version

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const month_id = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep',
 /*************************************/
 var calendar2021 = {jan: {1: "Сайхан амарна"}, feb: {1: "Сагсны тэмцээнтэй", 3: "Шагнал гардуулна даа", 17: "Жавхлан багшийн лаб 2-ыг хийнэ"}, mar: {2: "Энэ лабынхаа хугацааг сунгах уу яах вэ гэдэгээ шийднэ", 6: "Энд юу бичье дээ байз", 8: "Эмэгтэйчүүддээ баяр хүргэнэ дээ"}, apr: {1: "Бүгдээрээ худлаа ярьцаагаагаарай"}, may: {10: "Энэ сард ч ёстой юу ч болдоггүй сар даа"}, jun: {6: "Жавхлан багшийн төрсөн өдөр"},  jul: {4: "Хичээл амарсаан ураа"}, aug: {1: "Хөдөө явдаг цаг даа", 25: "Хичээл сонголт эхэллээ"}, sep: {1: "9-н сарын нэгэн боллоо ерөөсөө бидний баяр даа"}, oct: {13: "Сур сур бас дахин сур"}, nov: {2: "Сурсаар л бай"}, dec: {20: "Өвлийн семистер хаагдах нь дээ", 30: "Дүн гаргаж дууслаа баярлалаа баяртай"} }
 /************************************/
-const {jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec} = calendar2021;
-console.log(feb);
 console.table(calendar2021);
 isLeapYear = (year) => {
     return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
@@ -16,12 +14,17 @@ getFebDays = (year) => {
     return isLeapYear(year) ? 29 : 28
 }
 
+let description = document.querySelector('.ul-desc');
+description.innerHTML = '';
+
 generateCalendar = (month, year) => {
     
     let calendar_days = document.getElementById(month_id[month]);
     let calendar_year = document.getElementById('year');
+    
     calendar_days.innerHTML = '';
     calendar_year.innerHTML = year;
+    
 
     let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
@@ -38,11 +41,20 @@ generateCalendar = (month, year) => {
                             <span></span>`
             if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
                 day.classList.add('curr-date')
+
+                let list_item = document.createElement('li')
+                list_item.classList.add('curr-date')
+                list_item.innerHTML = `${month_id[month].toUpperCase()}-${currDate.getDate()}: Өнөөдөр`
+                description.appendChild(list_item)
             }
 
             for (const property in calendar2021[month_id[month]]) {
                 if (i - first_day.getDay() + 1 === parseInt(property)) {
-                    day.classList.add('ntf-date')
+                    day.classList.add('ntf-date');
+
+                    let list_item = document.createElement('li')
+                    list_item.innerHTML = `${month_id[month].toUpperCase()}-${property}: ${calendar2021[month_id[month]][property]}`
+                    description.appendChild(list_item)
                 }
             }
         }
@@ -63,11 +75,13 @@ function generateAll () {
 
 document.querySelector('#prev-year').onclick = () => {
     --curr_year.value;
+    description.innerHTML = '';
     generateAll();
 }
 
 document.querySelector('#next-year').onclick = () => {
     ++curr_year.value;
+    description.innerHTML = '';
     generateAll();
 }
 
